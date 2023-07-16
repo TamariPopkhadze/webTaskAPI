ab8d566ef74b8d7b4f846b9a1adab083925fbbd8
Tamar Popkhadze <popkhadze.tamar@kiu.edu.ge>
2023-07-16 14:08:11 +0400

    api commit

---
+++ src/controllers/deleteProduct.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import {
+  ProductWithBook,
+  ProductWithDVD,
+  ProductWithFurniture,
+} from "../model/productModel";
+
+export const deleteProducts = async (req: Request, res: Response) => {
+  const { body } = req;
+  const deletedProducts = [];
+  
+  try {
+    for (const id of body) {
+      let product = await ProductWithDVD.findOneAndDelete({ id });
+      if (!product) {
+        product = await ProductWithBook.findOneAndDelete({ id });
+      }
+      if (!product) {
+        product = await ProductWithFurniture.findOneAndDelete({ id });
+      }
+      if (!product) {
+        return res.status(404).json(`ID:${id} is not found`);
+      }
+      deletedProducts.push(product);
+    }
+
+    if (deletedProducts.length > 0) {
+      return res.status(200).json({
+        message: "Successfully deleted",
+        deletedProducts,
+      });
+    } else {
+      return res.status(404).json("No products found for deletion");
+    }
+  } catch (error) {
+    return res.status(500).json("Error occurred while deleting products");
+  }
+};
